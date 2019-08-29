0bc35cfa7eac34d7f631ab65ef94a6456cc24809
Hessah95 <hessah.alhamadi.95@gmail.com>
2019-08-29 09:51:59 +0300

    first commit

--- classrooms/urls.py
+++ classrooms/urls.py
@@ -11,10 +11,15 @@ urlpatterns = [
     path('classrooms/<int:classroom_id>/', views.classroom_detail, name='classroom-detail'),
 
     path('classrooms/create', views.classroom_create, name='classroom-create'),
+    path('classrooms/student/<int:classroom_id>/', views.student_create, name='student-create'),
     path('classrooms/<int:classroom_id>/update/', views.classroom_update, name='classroom-update'),
     path('classrooms/<int:classroom_id>/delete/', views.classroom_delete, name='classroom-delete'),
+
+    path('signup/',views.signup ,name='signup'),
+    path('signin/',views.signin ,name='signin'),
+    path('signout/',views.signout ,name='signout'),
 ]
 
 if settings.DEBUG:
-	urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
